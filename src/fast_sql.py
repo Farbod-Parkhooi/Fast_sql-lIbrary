bef74897b4d2c3b556eb11cb03b0e82f44f6f14c
Farbod Parkhuee <farbod.p1390@gmail.com>
2024-05-29 12:38:51 +0330

    rename __close__ function to close, create connect function, update __init__ function

--- src/fast_sql.py
+++ src/fast_sql.py
@@ -9,24 +9,32 @@ def convert_attr_to_text(attributes):
     return output
 class fast_sql():
     def __init__(self, db_name: str, table_name: str, attributes: list):
-        # Import library
-        import sqlite3 as sql
-        # Create values
-        connect = sql.connect(db_name)
-        cursor = connect.cursor()
-        str_attributes = convert_attr_to_text(attributes)
-        # Create Table in database
-        cursor.execute(f"""CREATE TABLE IF NOT EXISTS {table_name} ({str_attributes});""")
-        # Commit changes
-        connect.commit()
         # Create self values
         self.db_name = db_name
         self.tb_name = table_name
         self.attributes = attributes
+    def connect(self):
+        # Import library
+        import sqlite3 as sql
+        # Create values
+        connect = sql.connect(self.db_name)
+        cursor = connect.cursor()
+        str_attributes = convert_attr_to_text(self.attributes)
+        # Create Table in database
+        cursor.execute(f"""CREATE TABLE IF NOT EXISTS {self.tb_name} ({str_attributes});""")
+        # Commit changes
+        connect.commit()
+        # add more self values
         self.sql = sql
         self.con = connect
         self.cursor = cursor
         self.str_attributes = str_attributes
+    def close(self):
+        try: 
+            self.con.close()
+            return True
+        except: 
+            return False
     def Insert_in(self, values: list):
         if len(self.attributes) != len(values):
             return "Error; Len of attributes not equal to values len. Please check it."
@@ -36,9 +44,3 @@ class fast_sql():
                                 VALUES ({values})""")
             self.con.commit()
             return True
-    def __close__(self):
-        try: 
-            self.con.close()
-            return True
-        except: 
-            return False
